b959588c95403cd467a997331e4013df409f4f92
salahalhadeiedris <salahalhadei2016@gmail.com>
2025-11-28 18:39:16 +0200

    Disble collect

--- dwa/settings.py
+++ dwa/settings.py
@@ -132,7 +132,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
-
+DISBLE_COLLECTSTATIC = 1
 STATIC_URL = '/static/'
 # Media files
 import os
