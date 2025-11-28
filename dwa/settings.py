7bd1e5731cf64f992884a8d8b0962cefd17a9a9e
salahalhadeiedris <salahalhadei2016@gmail.com>
2025-11-28 05:08:27 +0200

    edit in settings

--- dwa/settings.py
+++ dwa/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False').lower() == 'True'
 
-ALLOWED_HOSTS = ['*', 'railway.app']
+ALLOWED_HOSTS = ['*', 'railway.app', '249dwa-production.up.railway.app']
 
 
 # Application definition
