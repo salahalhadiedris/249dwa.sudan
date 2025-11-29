cb2f159f71ddb2966c9124f5946765c3c89fa791
salahalhadeiedris <salahalhadei2016@gmail.com>
2025-11-29 19:36:08 +0200

    DB

--- dwa/settings.py
+++ dwa/settings.py
@@ -82,19 +82,15 @@ WSGI_APPLICATION = 'dwa.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-if 'DATABASE_URL' in os.environ:
-  DATABASES = {
-      'default': dj_database_url.config(
-          default=os.getenv('DATABASE_URL', 'sqlite:///db.sqlite3'),
-            conn_max_age=600,
-            ssl_require=True
-      )
-    }
-else:
-  DATABASES = {
+
+DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
     }
 }
 
