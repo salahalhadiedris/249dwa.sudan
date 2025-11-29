5dd214cf0f1268f1852d87e73d2c03064db49214
salahalhadeiedris <salahalhadei2016@gmail.com>
2025-11-29 20:47:19 +0200

    DB 1

--- dwa/settings.py
+++ dwa/settings.py
@@ -86,11 +86,11 @@ WSGI_APPLICATION = 'dwa.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT'),
+        'NAME': os.getenv("DB_NAME"),
+        'USER': os.getenv("DB_USER"),
+        'PASSWORD': os.getenv("DB_PASSWORD"),
+        'HOST': os.getenv("DB_HOST"),
+        'PORT': os.getenv("DB_PORT"),
     }
 }
 
