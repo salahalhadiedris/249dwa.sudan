3f9babc1671ef671c9d438b5aa20abe150df0457
salahalhadeiedris <salahalhadei2016@gmail.com>
2025-12-02 21:12:10 +0200

    editbatabase

--- dwa/settings.py
+++ dwa/settings.py
@@ -11,8 +11,9 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 import os
-import dj_database_url
 from pathlib import Path
+from datetime import timedelta
+import dj_database_url
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -30,7 +31,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False').lower() == 'True'
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS'), ".railway.app", ".up.railway.app", "127.0.0.1", "localhost",
 
 
 # Application definition
@@ -83,15 +84,10 @@ WSGI_APPLICATION = 'dwa.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv("DB_NAME"),
-        'USER': os.getenv("DB_USER"),
-        'PASSWORD': os.getenv("DB_PASSWORD"),
-        'HOST': os.getenv("DB_HOST"),
-        'PORT': os.getenv("DB_PORT"),
-    }
+DATABASE = os.getenv('DATABASE_URL')
+
+DATABASE = {
+    'default': dj_database_url.parse(DATABASE, conn_max_age=600, ssl_require=True)
 }
 
 
