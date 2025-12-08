abb7ef6e63afc60def2282f64ade50fa8f8dacb1
salahalhadeiedris <salahalhadei2016@gmail.com>
2025-12-08 04:04:38 +0200

    db

--- dwa/settings.py
+++ dwa/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = ('249dwa.pythonanywhere.com', '127.0.0.1')
+ALLOWED_HOSTS = ('249Dwa.pythonanywhere.com', '127.0.0.1')
 
 # Application definition
 
