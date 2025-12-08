f7450a447820bdef41e61ea6c6eda43072d0e0be
salahalhadeiedris <salahalhadei2016@gmail.com>
2025-12-08 04:16:04 +0200

    db

--- dwa/settings.py
+++ dwa/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = 'django-insecure-290nb%evht)5f4vc29%zaw*1p0zyoqi)v_4@u%&-@s0==ho@0a'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
