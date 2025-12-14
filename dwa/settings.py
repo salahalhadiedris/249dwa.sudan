f23f1c9de03433d3ac68b4d82d4ab17a4dc9c549
salahalhadeiedris <salahalhadei2016@gmail.com>
2025-12-14 18:53:53 +0200

    db

--- dwa/settings.py
+++ dwa/settings.py
@@ -20,7 +20,6 @@ load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-sys.path.insert(0, str(BASE_DIR / 'apps'))
 
 
 # Quick-start development settings - unsuitable for production
