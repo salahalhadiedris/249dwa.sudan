be3138cdb7a36b8a7a5e4312d1ea76f80c3f4d74
salahalhadeiedris <salahalhadei2016@gmail.com>
2025-12-14 19:21:41 +0200

    static root

--- dwa/settings.py
+++ dwa/settings.py
@@ -132,3 +132,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'staticfiles', 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = (BASE_DIR, 'dwa/static')
